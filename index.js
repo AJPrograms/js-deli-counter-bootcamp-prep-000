6d4113ab4d7ebc0c24dd82b45cf57c7db6b1f2bf
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 05:46:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var katzDeli = [];
 
 function takeANumber(katsDeliLine, name){
-  katsDeliLine.push(name)
-  return ``
+  katsDeliLine.push(name);
+  return `Welcome, ${name}. You are number ${katsDeliLine.indexOf(name) + 1} in line.`;
 }

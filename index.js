0228e3168761a5dd8dc10395691020107e0ac91e
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 05:52:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 var katzDeli = [];
 
-function takeANumber(katsDeliLine, name){
+function takeANumber(katzDeliLine, name) {
   katsDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katsDeliLine.indexOf(name) + 1} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length != 0) {
+    
+  }
+  else {
+    return `Currently serving ${katzDeliLine.shift()}.`;
+  }
 }

e5f0d5c29a1674d8f1a0c4a673b1d61ad1d26315
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 06:08:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,12 +8,17 @@ function takeANumber(katzDeliLine, name) {
 function nowServing(katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return `There is nobody waiting to be served!`;
-  }
-  else {
+  }  else {
     return `Currently serving ${katzDeliLine.shift()}.`;
   }
 }
 
 function currentLine(katzDeliLine) {
-  return currentLine;
+  var n = 0;
+  if (katzDeliLine.length === 0) {
+    return `The line is currently empty.`;
+  } else if (n < katzDeliLine.length) {
+    n++;
+    return `The line is currently: ${n}. ${katzDeliLine[n]}`;
+  }
 }

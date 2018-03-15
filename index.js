631e6b60a3798ecc2e5365a7edb27e8c61d17559
Amanda Meyer <amandajaymeyer@gmail.com>
2018-03-15 06:47:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,5 +14,14 @@ function nowServing(katzDeliLine) {
 }
 
 function currentLine (katzDeliLine) {
-  
+  var n = 0;
+  if (katzDeliLine.length === 0) {
+    return `The line is currently empty`;
+  }
+  else {
+    while (n < katzDeliLine.length) {
+      n++;
+    }
+    return `The line is currently: ${n + 1}. ${katzDeliLine[n]}`;
+  }
 }
